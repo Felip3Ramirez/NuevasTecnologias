67c545f8168b0c2f0ece6f52705479e63ed56c5a
Felip3 <juanfeliperamirezcardona0@gmail.com>
2025-08-12 20:30:15 -0500

    Create main.py

---
+++ ProyectosBasicosPython/1PYTHON/main.py
@@ -0,0 +1,28 @@
+#Variables
+nombreJugador='Edwin Cardona'
+posicionJugador='Medio Campista'
+numeroCamiseta=10
+estaturaJugador=1.86
+salarioMensual=200000000
+esLeyendaDelClub=True
+numeroTitulosEn2025=None 
+
+'''nombreDelEstadio=input("Digita el nombre del estadio: ")   
+ciudadPartido=input("Digita la ciudad del partido: ")
+numerosAsistentesPartido=input("Cuantas personas asistieron al partido: ")'''
+
+numeroUno=int(input("Digita un numero: "))
+numeroDos=int(input("Digita un numero: "))
+suma=numeroUno+numeroDos
+print(f"{numeroUno}+{numeroDos}={suma}")
+
+#Arreglos(Listas)
+
+#Objetos(Diccionarios)
+
+#Salidas por consola
+
+'''print(f"Datos del partido Estadio: {nombreDelEstadio}, Ciudad: {ciudadPartido}, Asistentes partido: {numerosAsistentesPartido} .")
+print("Nombre del estadio: " +nombreDelEstadio)
+print("Ciudad del partido: " + ciudadPartido)
+print("Numero de asistentes: " + numerosAsistentesPartido)'''
